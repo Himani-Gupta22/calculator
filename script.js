fcf3c9091e0a80ee27693760111f0df626a7f76e
Himani-Gupta22 <141930223+Himani-Gupta22@users.noreply.github.com>
2023-08-11 18:49:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    if (value === '←') {
+        display.value = display.value.slice(0, -1); 
+    } else {
+        display.value += value;
+    }
+}
+
+
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
